4d9bd5b9e1404d3f81fdad48bf226e2c24d10ae1
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2022-04-27 00:32:45 +0200

    Fetch et images

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,14 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    const getPhotographers = async () => {
+        return fetch('./data/photographers.json') // Get .json
+            .then(response => response.json()) // Make JS object
+            .then (data => {
+                return data;
+            });
     }
 
-    async function displayData(photographers) {
+
+    function displayData(photographers) {
+    console.log("Declanché")
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
@@ -40,6 +23,5 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
-    
-    init();
-    
+
+    init()
